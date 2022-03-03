122c9be176366c9977fab378e0346a512a476496
wly <1355498705@qq.com>
2022-03-03 20:50:14 +0800

    npmignore  配置

--- script/lib.config.ts
+++ script/lib.config.ts
@@ -18,12 +18,16 @@ export default defineConfig({
   ],
   build: {
     lib: {
+      // 设置入口文件
       entry: path.resolve(__dirname, "../packages/index.ts"),
       name: "xbom-lib",
       fileName: (format: string) => `xbom-lib.${format}.js`,
-      formats: ['es', 'umd', 'cjs']
+      formats: ['es', 'umd', 'cjs'],
+  
     },
+    sourcemap: true, // 输出.map文件
     rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
       external: Object.keys(require("../package.json").peerDependencies),
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
